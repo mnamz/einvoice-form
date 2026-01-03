a9beb09c1c82919df41d4644fe3a2d064b328ab6
aliifz <mnamz101@gmail.com>
2026-01-03 16:22:25 +0800

    update config

---
+++ src/config/branches.js
@@ -0,0 +1,18 @@
+export const branches = [
+  {
+    id: 'branch1',
+    name: 'Branch 1',
+    endpoint: 'https://api.example.com/branch1/invoice'
+  },
+  {
+    id: 'branch2',
+    name: 'Branch 2',
+    endpoint: 'https://api.example.com/branch2/invoice'
+  },
+  {
+    id: 'branch3',
+    name: 'Branch 3',
+    endpoint: 'https://api.example.com/branch3/invoice'
+  }
+]
+
